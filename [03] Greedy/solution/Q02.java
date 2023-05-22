e952537dd85ec012fc7fa71ce6b3a21435c9dd52
spacewalk00 <ju001217@gmail.com>
2023-05-23 02:18:54 +0900

    [ADD] Q02.java

---
+++ [03] Greedy/solution/Q02.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MyClass {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        
+        char[] strArr = str.toCharArray(); 
+        int size = strArr.length;
+        
+        int sum = 0;
+        for(int i = 0; i < size; i++) {
+            if(i == 0) {
+                sum = strArr[i] - '0';
+            }
+            else if(i != 0 && strArr[i-1] == '0') {
+                sum = sum + (strArr[i] - '0');
+            } 
+            else {
+                sum = sum * (strArr[i] - '0');
+            }
+        }
+        System.out.println(sum);
+    }
+}
