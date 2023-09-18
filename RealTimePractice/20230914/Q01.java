21212a7443dd443e21369121e630116757fbb6b5
spacewalk00 <ju001217@gmail.com>
2023-09-18 21:24:14 +0900

    [FIX] 라이브 코데 - Q01.java

--- RealTimePractice/20230914/Q01.java
+++ RealTimePractice/20230914/Q01.java
@@ -1,24 +1,25 @@
 /**
- * 카카오 키패드 누르기
+ * 카카오 2020 키패드 누르기
+ * LEVEL 1
  * https://school.programmers.co.kr/learn/courses/30/lessons/67256
- * 미완성
+ * 
+ * 정확성: 100.0
+ * 합계: 100.0 / 100.0
+ * 
+ * 1차 풀이: '이동하는 코드 누락'
+ * 2차 풀이: 성공
  */
 class Solution {
-    //public static final int dx[] = {0, 0, -1, 1}; //상하좌우
-    //public static final int dy[] = {-1, 1, 0, 0};
+    public static final int[][] location = {
+        {0,0}, {0,1}, {0,2}, 
+        {1,0}, {1,1}, {1,2}, 
+        {2,0}, {2,1}, {2,2},
+        {3,0}, {3,1}, {3,2}
+    };
     
     public String solution(int[] numbers, String hand) {
         StringBuilder result = new StringBuilder();
-        
-        int[][] keypad = new int[3][4];
-        int value = 1;
-        for(int i = 0; i < 3; i++) {
-            for(int j=0; j < 4; j++) {
-                keypad[i][j] = value++;
-                //*:10 0: 11 #:12
-            }
-        }
-        
+
         //numbers check
         int[] now_left = {3, 0}; //*
         int[] now_right = {3, 2}; //#     
@@ -27,84 +28,64 @@ class Solution {
         for(int i=0; i<length; i++) {
             if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
                 result.append("L");
+                now_left[0] = location[numbers[i]-1][0];
+                now_left[1] = location[numbers[i]-1][1];
             }
             else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
                 result.append("R");
+                now_right[0] = location[numbers[i]-1][0];
+                now_right[1] = location[numbers[i]-1][1];
             }
             else {
                 //2, 5, 8, 0(11)
-                //거리 구하기
                 int[] temp = new int[2];
                 switch(numbers[i]) {
                     case 2: 
-                        temp[0] = 0;
-                        temp[1] = 1;
-                        //temp = {0, 1};
+                        temp[0] = 0; temp[1] = 1;
+                        //temp = {0, 1}; illegal start of expression
                         break;
                     case 5:
-                        temp[0] = 1;
-                        temp[1] = 1;
-                        //temp = {1, 1};
+                        temp[0] = 1; temp[1] = 1;
                         break;
                     case 8:
-                        temp[0] = 2;
-                        temp[1] = 1;
-                        //temp = {2, 1};
+                        temp[0] = 2; temp[1] = 1;
                         break;
                     case 0:
-                        temp[0] = 3;
-                        temp[1] = 1;
-                        //temp = {3, 1};
+                        temp[0] = 3; temp[1] = 1;
                         break;
                     default:
                         break;
                 }
-                int temp1 = temp[0] - now_left[0];                
+                //거리구하기 (절댓값 => Math.abs)
+                int temL = Math.abs(temp[0] - now_left[0]) + Math.abs(temp[1] - now_left[1]);
+                /*int temp1 = temp[0] - now_left[0];                
                 temp1 = temp1 < 0 ? (temp1) * (-1) : (temp1);
                 int temp2 = temp[1] - now_left[1];                
                 temp2 = temp2 < 0 ? (temp2) * (-1) : (temp2);
-                int temL = temp1 + temp2;
-                
-                
-                temp1 = temp[0] - now_right[0];                
-                temp1 = temp1 < 0 ? (temp1) * (-1) : (temp1);
-                temp2 = temp[1] - now_right[1];                
-                temp2 = temp2 < 0 ? (temp2) * (-1) : (temp2);
-                int temR = temp1 + temp2;
+                int temL = temp1 + temp2;*/
+                int temR = Math.abs(temp[0] - now_right[0]) + Math.abs(temp[1] - now_right[1]);
                 
                 if(temL < temR) {
                     result.append("L");
+                    now_left[0] = temp[0];
+                    now_left[1] = temp[1];
                 } else if(temL > temR) {
                     result.append("R");
+                    now_right[0] = temp[0];
+                    now_right[1] = temp[1];
                 } else {
                     if(hand.equals("left")) {
-                        result.append("L");           
+                        result.append("L");   
+                        now_left[0] = temp[0];
+                        now_left[1] = temp[1];
                     } else if(hand.equals("right")) {
                         result.append("R");
+                        now_right[0] = temp[0];
+                        now_right[1] = temp[1];
                     }
-                }
-                
-                /*
-                처음에 잘못 생각함: 좌표 거리로 구할 수 있다고 착각했음
-                double distance_l = Math.pow((temp[0] - now_left[0]), 2) + Math.pow((temp[1] - now_left[1]), 2);
-                double distance_r = Math.pow((temp[0] - now_right[0]), 2) + Math.pow((temp[1] - now_right[1]), 2);
-                
-                if(distance_l > distance_r) {
-                    result.append("L");
-                } else if(distance_l < distance_r) {
-                    result.append("R");
-                } else {
-                    if(hand.equals("left")) {
-                        result.append("L");           
-                    } else if(hand.equals("right")) {
-                        result.append("R");
-                    }
-                }*/
-                
-                
+                }                
             }
         }     
         return result.toString();
     }
-    
 }
